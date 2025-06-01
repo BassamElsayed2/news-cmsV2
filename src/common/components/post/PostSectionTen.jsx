7328d99fa188466afa7656351c47f21f15fdea7b
ElBoghdady <hadeerelboghdady54@gmail.com>
2025-06-01 16:14:25 +0300

    edit css  Latest News Section

--- src/common/components/post/PostSectionTen.jsx
+++ src/common/components/post/PostSectionTen.jsx
@@ -66,118 +66,122 @@ const PostSectionTen = () => {
 
   return (
     <div className="axil-post-grid-area axil-section-gap bg-color-white">
-      <div className="container">
-        <SectionTitleOne title={t("sectionTitle")} />
-        <div className="row">
-          <div className="col-lg-12">
-            <Tab.Container id="axilTab" activeKey={activeNav} onSelect={(k) => setActiveNav(k)}>
-              <Nav className="axil-tab-button nav nav-tabs mt--20">
-                {categories.map((cate) => (
-                  <Nav.Item key={slugify(cate)}>
-                    <Nav.Link eventKey={slugify(cate)}>
-                      {cate === "all" ? "All" : cate === "normal" ? "Normal" : cate}
-                    </Nav.Link>
-                  </Nav.Item>
-                ))}
-              </Nav>
+  <div className="container">
+    <SectionTitleOne title={t("sectionTitle")} />
+    <div className="row">
+      <div className="col-lg-12">
+        <Tab.Container id="axilTab" activeKey={activeNav} onSelect={(k) => setActiveNav(k)}>
+          <Nav className="axil-tab-button nav nav-tabs mt--20">
+            {categories.map((cate) => (
+              <Nav.Item key={slugify(cate)}>
+                <Nav.Link eventKey={slugify(cate)}>
+                  {cate === "all" ? "All" : cate === "normal" ? "Normal" : cate}
+                </Nav.Link>
+              </Nav.Item>
+            ))}
+          </Nav>
 
-              <Tab.Content className="grid-tab-content mt--10">
-                <Tab.Pane className="single-post-grid" eventKey={activeNav}>
-                  <div className="row mt--40">
-                    <div className="col-xl-5 col-lg-6 col-md-12 col-12">
-                      {tabPostData.slice(-5).map((data) => (
-                        <div className="content-block post-medium post-medium-border border-thin" key={data.slug}>
-                          <div className="post-thumbnail">
-                            <Link href={`/post/${data.slug}`}>
-                              <a>
-                                {getImageSrc(data.images) ? (
-                                  <Image
-                                    src={getImageSrc(data.images)}
-                                    alt={locale === "en" ? data.title_en : data.title_ar}
-                                    height={100}
-                                    width={100}
-                                    priority={true}
-                                  />
-                                ) : (
-                                  <div
-                                    style={{
-                                      width: "100px",
-                                      height: "100px",
-                                      backgroundColor: "#ccc",
-                                    }}
-                                  />
-                                )}
+          <Tab.Content className="grid-tab-content mt--10">
+            <Tab.Pane className="single-post-grid" eventKey={activeNav}>
+              <div className="row mt--40">
+                {/* العمود الصغير */}
+                <div className="col-xl-5 col-lg-6 col-md-12 col-12">
+                  {tabPostData.slice(-5).map((data) => (
+                    <div className="content-block post-medium post-medium-border border-thin" key={data.slug}>
+                      <div className="post-thumbnail">
+                        <Link href={`/post/${data.slug}`}>
+                          <a>
+                            {getImageSrc(data.images) ? (
+                              <Image
+                                src={getImageSrc(data.images)}
+                                alt={locale === "en" ? data.title_en : data.title_ar}
+                                height={100}
+                                width={100}
+                                priority={true}
+                              />
+                            ) : (
+                              <div
+                                style={{
+                                  width: "100px",
+                                  height: "100px",
+                                  backgroundColor: "#ccc",
+                                }}
+                              />
+                            )}
+                          </a>
+                        </Link>
+                      </div>
+                      <div className="post-content mt-2"> {/* مسافة بين الصورة والعنوان */}
+                        <div className="post-cat">
+                          <div className="post-cat-list">
+                            <Link href={`/category/${slugify(data.status || "")}`}>
+                              <a className="hover-flip-item-wrapper">
+                                <span className="hover-flip-item">
+                                  <span data-text={data.status || "Normal"}>{data.status || "Normal"}</span>
+                                </span>
                               </a>
                             </Link>
                           </div>
-                          <div className="post-content">
-                            <div className="post-cat">
-                              <div className="post-cat-list">
-                                <Link href={`/category/${slugify(data.status || "")}`}>
-                                  <a className="hover-flip-item-wrapper">
-                                    <span className="hover-flip-item">
-                                      <span data-text={data.status || "Normal"}>{data.status || "Normal"}</span>
-                                    </span>
-                                  </a>
-                                </Link>
-                              </div>
-                            </div>
-                            <h4 className="title">
-                              <Link href={`/post/${data.slug}`}>
-                                <a>{locale === "en" ? data.title_en : data.title_ar}</a>
-                              </Link>
-                            </h4>
-                          </div>
                         </div>
-                      ))}
+                        <h4 className="title text-truncate-2">
+                          <Link href={`/post/${data.slug}`}>
+                            <a>{locale === "en" ? data.title_en : data.title_ar}</a>
+                          </Link>
+                        </h4>
+                      </div>
                     </div>
-                    <div className="col-xl-7 col-lg-6 col-md-12 col-12 mt_md--40 mt_sm--40">
-                      <div className="content-block content-block post-grid post-grid-transparent">
-                        {getImageSrc(firstPost.images) && (
-                          <div className="post-thumbnail">
-                            <Link href={`/post/${firstPost.slug}`}>
-                              <a>
-                                <Image
-                                  src={getImageSrc(firstPost.images)}
-                                  alt={locale === "en" ? firstPost.title_en : firstPost.title_ar}
-                                  height={660}
-                                  width={705}
-                                  priority={true}
-                                />
+                  ))}
+                </div>
+
+                {/* العمود الكبير */}
+                <div className="col-xl-7 col-lg-6 col-md-12 col-12 mt_md--40 mt_sm--40">
+                  <div className="content-block content-block post-grid post-grid-transparent">
+                    {getImageSrc(firstPost.images) && (
+                      <div className="post-thumbnail">
+                        <Link href={`/post/${firstPost.slug}`}>
+                          <a>
+                            <Image
+                              src={getImageSrc(firstPost.images)}
+                              alt={locale === "en" ? firstPost.title_en : firstPost.title_ar}
+                              height={660}
+                              width={705}
+                              priority={true}
+                            />
+                          </a>
+                        </Link>
+                      </div>
+                    )}
+                    <div className="post-grid-content">
+                      <div className="post-content">
+                        <div className="post-cat">
+                          <div className="post-cat-list">
+                            <Link href={`/category/${slugify(firstPost.status || "")}`}>
+                              <a className="hover-flip-item-wrapper">
+                                <span className="hover-flip-item">
+                                  <span data-text={firstPost.status || "Normal"}>{firstPost.status || "Normal"}</span>
+                                </span>
                               </a>
                             </Link>
                           </div>
-                        )}
-                        <div className="post-grid-content">
-                          <div className="post-content">
-                            <div className="post-cat">
-                              <div className="post-cat-list">
-                                <Link href={`/category/${slugify(firstPost.status || "")}`}>
-                                  <a className="hover-flip-item-wrapper">
-                                    <span className="hover-flip-item">
-                                      <span data-text={firstPost.status || "Normal"}>{firstPost.status || "Normal"}</span>
-                                    </span>
-                                  </a>
-                                </Link>
-                              </div>
-                            </div>
-                            <h3 className="title">
-                              <Link href={`/post/${firstPost.slug}`}>
-                                <a>{locale === "en" ? firstPost.title_en : firstPost.title_ar}</a>
-                              </Link>
-                            </h3>
-                          </div>
                         </div>
+                        <h3 className="title">
+                          <Link href={`/post/${firstPost.slug}`}>
+                            <a>{locale === "en" ? firstPost.title_en : firstPost.title_ar}</a>
+                          </Link>
+                        </h3>
                       </div>
                     </div>
                   </div>
-                </Tab.Pane>
-              </Tab.Content>
-            </Tab.Container>
-          </div>
-        </div>
+                </div>
+              </div>
+            </Tab.Pane>
+          </Tab.Content>
+        </Tab.Container>
       </div>
     </div>
+  </div>
+</div>
+
   );
 };
 
