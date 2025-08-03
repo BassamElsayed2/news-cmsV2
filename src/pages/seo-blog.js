8f18cd7d7245d46cc9c08fa187c71d7409c69381
Bassam-Elsaied <bassame993@gmail.com>
2025-08-03 21:04:23 +0300

    remove galleryOne

--- src/pages/seo-blog.js
+++ src/pages/seo-blog.js
@@ -1,9 +1,9 @@
-import { useRouter } from 'next/router'
-import GalleryOne from "../common/gallery/InstagramOne";
+import { useRouter } from "next/router";
+
 import SocialOne from "../common/components/social/SocialOne";
 import FooterThree from "../common/elements/footer/FooterThree";
 import HeaderTwo from "../common/elements/header/HeaderTwo";
-import { getAllPosts } from '../../lib/api';
+import { getAllPosts } from "../../lib/api";
 import PostSectionFour from "../common/components/post/PostSectionFour";
 import PostSectionThree from "../common/components/post/PostSectionThree";
 import CategoryListSlide from "../common/components/category/CategoryListSlide";
@@ -11,60 +11,59 @@ import PostSectionSeven from "../common/components/post/PostSectionSeven";
 import PostSectionTwo from "../common/components/post/PostSectionTwo";
 import PostSectionEight from "../common/components/post/PostSectionEight";
 import HeadTitle from "../common/elements/head/HeadTitle";
-import { slugify } from '../common/utils';
+import { slugify } from "../common/utils";
+import GalleryOne from "../common/gallery/GalleryOne";
 
-const SeoBlog = ({allPosts}) => {
+const SeoBlog = ({ allPosts }) => {
   const router = useRouter();
   const PageSlug = router.pathname.replace("/", "");
-  
-  const seoPost = allPosts.filter(post => slugify(post.pCate) === PageSlug);
-  const videoPost = allPosts.filter(post => post.postFormat === "video");
 
+  const seoPost = allPosts.filter((post) => slugify(post.pCate) === PageSlug);
+  const videoPost = allPosts.filter((post) => post.postFormat === "video");
 
-    return ( 
-        <>
-        <HeadTitle pageTitle="SEO Blog" />
-			<HeaderTwo postData={allPosts}/>
-			<PostSectionEight postData={seoPost} />
-			<PostSectionTwo 
-			postData={allPosts} 
-			adBanner={true} 
-			headingTitle="What's new at Bloger"
-			/>
-			<PostSectionSeven postData={allPosts} />
-      <PostSectionThree postData={videoPost} heading="Featured Video"/>    
+  return (
+    <>
+      <HeadTitle pageTitle="SEO Blog" />
+      <HeaderTwo postData={allPosts} />
+      <PostSectionEight postData={seoPost} />
+      <PostSectionTwo
+        postData={allPosts}
+        adBanner={true}
+        headingTitle="What's new at Bloger"
+      />
+      <PostSectionSeven postData={allPosts} />
+      <PostSectionThree postData={videoPost} heading="Featured Video" />
       <CategoryListSlide cateData={allPosts} />
-      <PostSectionFour postData={seoPost} adBanner={true}/>
+      <PostSectionFour postData={seoPost} adBanner={true} />
       <SocialOne />
-      <GalleryOne parentClass="bg-color-grey"/>
-			<FooterThree />
-        </>
-     );
-}
- 
-export default SeoBlog;  
+      <GalleryOne parentClass="bg-color-grey" />
+      <FooterThree />
+    </>
+  );
+};
 
+export default SeoBlog;
 
 export async function getStaticProps() {
-    const allPosts = getAllPosts([
-      'id',
-      'title',
-      'featureImg',
-      'postFormat',
-      'featured',
-      'date',
-      'slug',
-      'pCate',
-      'cate',
-      'cate_img',
-      'author_img',
-      'author_name',
-      'post_views',
-      'read_time',
-      'author_social',
-    ])
-  
-    return {
-      props: { allPosts }
-    }
-  }
+  const allPosts = getAllPosts([
+    "id",
+    "title",
+    "featureImg",
+    "postFormat",
+    "featured",
+    "date",
+    "slug",
+    "pCate",
+    "cate",
+    "cate_img",
+    "author_img",
+    "author_name",
+    "post_views",
+    "read_time",
+    "author_social",
+  ]);
+
+  return {
+    props: { allPosts },
+  };
+}
